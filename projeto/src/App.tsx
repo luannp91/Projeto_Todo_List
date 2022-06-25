c6aeb0c002b364acb106c5af7e2ce5e14d8a8dee
Luan Pinto <luan.nelmo@hotmail.com>
2022-06-25 20:22:55 -0300

    Criando Formulario

--- projeto/src/App.tsx
+++ projeto/src/App.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import styles from "./App.module.css";
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div>
       <Header/>
       <main className={ styles.main }>
-        <h1>Conteúdo</h1>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList/>
+        </div>
       </main>
       <Footer/>
     </div>
---
+++ projeto/src/components/TaskList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const TaskList = (props: Props) => {
+    return (
+        <div>
+            <p>lista de tarefas</p>
+        </div>
+    )
+}
+
+export default TaskList
