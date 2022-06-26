536b36c11f92a4dbedeb1a35d15006ccde82b377
Luan Pinto <luan.nelmo@hotmail.com>
2022-06-25 22:12:49 -0300

    Removendo Tarefas

--- projeto/src/components/TaskList.tsx
+++ projeto/src/components/TaskList.tsx
@@ -4,9 +4,10 @@ import styles from "./TaskList.module.css";
 
 interface Props {
     taskList: ITask[];
+    handleDelete(id: number): void;
 }
 
-const TaskList = ({ taskList }: Props) => {
+const TaskList = ({ taskList, handleDelete }: Props) => {
     return (
         <>
             { taskList.length > 0 ? (
@@ -18,7 +19,7 @@ const TaskList = ({ taskList }: Props) => {
                         </div>
                         <div className={ styles.actions }>
                             <i className="bi bi-pencil"></i>
-                            <i className="bi bi-trash"></i>
+                            <i className="bi bi-trash" onClick={ () => { handleDelete(task.id) }}></i>
                         </div>
                     </div>
                 ))
