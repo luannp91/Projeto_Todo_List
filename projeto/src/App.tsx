82fd59553f144d9de99a120a1b68aa42c57ed104
Luan Pinto <luan.nelmo@hotmail.com>
2022-06-26 19:25:26 -0300

    Evento para fechar Modal

--- projeto/src/App.tsx
+++ projeto/src/App.tsx
@@ -19,6 +19,20 @@ function App() {
     )
   }
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal")
+    if (display) {
+      modal!.classList.remove("hide")
+    }
+    else {
+      modal!.classList.add("hide")
+    }
+  }
+
+  const editTask = (): void => {
+    hideOrShowModal(true)
+  }
+
   return (
     <div>
       <Modal children={ <TaskForm btnText="Editar Tarefa" taskList={ taskList }/>} />
@@ -30,7 +44,7 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={ taskList } handleDelete={ deleteTask } />
+          <TaskList taskList={ taskList } handleDelete={ deleteTask } handleEdit={ editTask } />
         </div>
       </main>
       <Footer/>
