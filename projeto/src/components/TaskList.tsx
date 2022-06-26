761b219534f480019cd67b529a79cd6fe6417bc6
Luan Pinto <luan.nelmo@hotmail.com>
2022-06-25 21:41:58 -0300

    HTML dos itens da lista

--- projeto/src/components/TaskList.tsx
+++ projeto/src/components/TaskList.tsx
@@ -12,7 +12,14 @@ const TaskList = ({ taskList }: Props) => {
             { taskList.length > 0 ? (
                 taskList.map((task) => (
                     <div key={ task.id }>
-                        <p>{ task.title }</p>
+                        <div>
+                            <h4>{ task.title }</h4>
+                            <p>Dificuldade: { task.difficulty }</p>
+                        </div>
+                        <div>
+                            <i className="bi bi-pencil"></i>
+                            <i className="bi bi-trash"></i>
+                        </div>
                     </div>
                 ))
             ) : (
