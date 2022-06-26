d275600c4850da85de4edc7208a8b0275c4ee9cc
Luan Pinto <luan.nelmo@hotmail.com>
2022-06-25 21:20:06 -0300

    Adicionando Tarefas

--- projeto/src/App.tsx
+++ projeto/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import styles from "./App.module.css";
@@ -7,13 +7,16 @@ import TaskList from './components/TaskList';
 import { ITask } from './interfaces/Task';
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div>
       <Header/>
       <main className={ styles.main }>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa" />
+          <TaskForm btnText="Criar Tarefa" taskList={ taskList } setTaskList={ setTaskList } />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
--- projeto/src/components/TaskForm.tsx
+++ projeto/src/components/TaskForm.tsx
@@ -4,15 +4,28 @@ import { ITask } from "../interfaces/Task";
 
 interface Props {
     btnText: string
+    taskList: ITask[];
+    setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
 }
 
-const TaskForm = ({ btnText }: Props) => {
+const TaskForm = ({ btnText, taskList, setTaskList }: Props) => {
 
     const [id, setId] = useState<number>(0);
     const [title, setTitle] = useState<string>("");
     const [difficulty, setDifficulty] = useState<number>(0);
 
-    const addTaskHandler = () => {}
+    const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const id = Math.floor(Math.random() * 1000)
+
+        const newTask: ITask = { id, title, difficulty }
+
+        setTaskList!([...taskList, newTask])
+
+        setTitle("")
+        setDifficulty(0)
+    }
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.name === "title") {
@@ -27,11 +40,11 @@ const TaskForm = ({ btnText }: Props) => {
     <form onSubmit={ addTaskHandler } className={ styles.form }>
         <div className={ styles.input_container }>
             <label htmlFor="title">Título:</label>
-            <input type="text" name="title" placeholder="Título da tarefa" onChange={ handleChange } />
+            <input type="text" name="title" placeholder="Título da tarefa" onChange={ handleChange } value={ title } />
         </div>
         <div className={ styles.input_container }>
             <label htmlFor="difficulty">Dificuldade:</label>
-            <input type="text" name="difficulty" placeholder="Dificuldade da tarefa" onChange={ handleChange } />
+            <input type="text" name="difficulty" placeholder="Dificuldade da tarefa" onChange={ handleChange } value={ difficulty } />
         </div>
         <input type="submit" value={ btnText } />
     </form>
